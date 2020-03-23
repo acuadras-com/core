d401a12b0fcc7c25cd1eba31d92a5a1ef3152a7e
espinosa4-sofi <jespinosa@sofi.org>
2020-03-23 09:18:26 -0500

    creating base project

---
+++ tutendero/src/main/kotlin/com/tutendero/TutenderoApplication.kt
@@ -0,0 +1,11 @@
+package com.tutendero
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class TutenderoApplication
+
+fun main(args: Array<String>) {
+	runApplication<TutenderoApplication>(*args)
+}
---
+++ tutendero/src/test/kotlin/com/tutendero/TutenderoApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.tutendero
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class TutenderoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
